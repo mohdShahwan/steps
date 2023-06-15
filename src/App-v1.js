30d1ae6e825d1f54381ebe8134f10366f803752f
mohdShahwan <mshahwan002@gmail.com>
2023-06-15 09:55:03 +0300

    Making a reusable button + using 'children' prop

--- src/App-v1.js
+++ src/App-v1.js
@@ -46,36 +46,21 @@ function Steps() {
           </p>
 
           <div className="buttons">
-            <Button
-              textColor={"#fff"}
-              bgColor={"#7950f2"}
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handlePrevious}
-              text={"Previous"}
             >
-              <span>👈</span> Previous
-            </Button>
-            <Button
-              textColor={"#fff"}
-              bgColor={"#7950f2"}
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handleNext}
-              text={"Next"}
             >
-              Next <span>👉</span>
-            </Button>
+              Next
+            </button>
           </div>
         </div>
       )}
     </div>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }
